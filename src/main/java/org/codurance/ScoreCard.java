e23fe5b8c385d7dd01459d731f4b7f6c0e6016cf
Hassan <hassansaleem194@gmail.com>
2022-09-06 15:35:05 +0100

    refactored score count

--- src/main/java/org/codurance/ScoreCard.java
+++ src/main/java/org/codurance/ScoreCard.java
@@ -6,20 +6,16 @@ import java.util.Arrays;
 public class ScoreCard {
     public static Integer bowling(String[] player1) {
 
-        Integer score = 0;
+        int score = 0;
         for (String game : player1
         ) {
-            if (game.charAt(0) == '-') {
-                score += 0;
-            } else {
-                score += Integer.parseInt(String.valueOf(game.charAt(0)));
-            }
 
-            if (game.charAt(1) == '-') {
-                score += 0;
-            } else {
-                score += Integer.parseInt(String.valueOf(game.charAt(1)));
-            }
+            for (int i=0; i<game.length(); i++)
+                if (game.charAt(i) == '-') {
+                    score += 0;
+                } else {
+                    score += Integer.parseInt(String.valueOf(game.charAt(i)));
+                }
         }
 
         return score;
